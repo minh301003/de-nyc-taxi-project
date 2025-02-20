e703a1a744e2cbe210249a3220ea05bd76fe10be
minh301003 <chunghoangminhtb@gmail.com>
2025-02-20 16:20:35 +0700

    add dataproc v5

--- terraform/main.tf
+++ terraform/main.tf
@@ -138,15 +138,15 @@ resource "google_dataproc_cluster" "dataproc_cluster" {
       num_instances = 1
       machine_type  = "n1-standard-4"
       disk_config {
-        boot_disk_size_gb = 10
+        boot_disk_size_gb = 30
       }
     }
 
     worker_config {
-      num_instances = 2
+      num_instances = 1
       machine_type  = "n1-standard-4"
       disk_config {
-        boot_disk_size_gb = 10
+        boot_disk_size_gb = 30
       }
     }
 
